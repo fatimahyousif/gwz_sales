136334eb40a809779ce11bb5e8e7713eddd3f88f
Fatimah Althaqib <sunandmoon_321@hotmail.com>
2024-03-04 15:35:06 +0300

    Update gwz_sales.sql

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -1,8 +1,8 @@
 
 SELECT 
   date_date AS sale_date,
-  SUM(turnover) AS daily_turnover,
-  SUM(purchase_cost) AS daily_purchase_cost
+ ROUND(SUM(turnover),2)  AS daily_turnover,
+ ROUND(SUM(purchase_cost),1) AS daily_purchase_cost
 FROM 
   `fatiamh.course14.gwz_sales_new`
 GROUP BY 
